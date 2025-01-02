70f65550842fcd8b6a83763d8fd62a1ef0e79b9a
Nikita <zhni23u172@student.bmstu.ru>
2025-01-02 22:07:37 +0300

    Add ticks measurement via `rdtsc`

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <inttypes.h>
 
 int fib(int n);
 int do_stuff(void);
+uint64_t tick(void);
 
 int main(void)
 {
     int n = 11;
+    uint64_t start, end;
+
+    start = tick();
     printf("fib(%d) = %d\n", n, fib(n));
+    end = tick();
+
+    printf("It took %" PRIu64 " ticks.\n", end - start);
 
     printf("Do_stuff:\n");
     return do_stuff();
